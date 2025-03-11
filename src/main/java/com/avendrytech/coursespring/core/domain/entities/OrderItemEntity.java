65154acdd0486cc897249b8e0c96ff583d48ac46
mvcode <mateus.13061903@gmail.com>
2025-03-11 16:14:25 -0300

    Aula 277 - Parte 1
    * Contruindo a concatenação de duas tabelas com valores adicionais

---
+++ src/main/java/com/avendrytech/coursespring/core/domain/entities/OrderItemEntity.java
@@ -0,0 +1,78 @@
+package com.avendrytech.coursespring.core.domain.entities;
+
+import com.avendrytech.coursespring.core.domain.entities.pk.OrderItemPk;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItemEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPk id;
+
+    private Integer quantity;
+
+    private Double price;
+
+    public OrderItemEntity(){}
+
+    public OrderItemEntity(OrderEntity order, ProductEntity product, Integer quantity, Double price) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public OrderEntity getOrder() {
+        return this.id.getOrder();
+    }
+
+    public void setOrder(OrderEntity order) {
+        this.id.setOrder(order);
+    }
+
+    public ProductEntity getProduct() {
+        return this.id.getProduct();
+    }
+
+    public void setProduct(ProductEntity product) {
+        this.id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntity that = (OrderItemEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/avendrytech/coursespring/core/domain/entities/pk/OrderItemPk.java
@@ -0,0 +1,54 @@
+package com.avendrytech.coursespring.core.domain.entities.pk;
+
+import com.avendrytech.coursespring.core.domain.entities.OrderEntity;
+import com.avendrytech.coursespring.core.domain.entities.ProductEntity;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+
+public class OrderItemPk implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private OrderEntity order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity product;
+
+    public OrderEntity getOrder() {
+        return order;
+    }
+
+    public void setOrder(OrderEntity order) {
+        this.order = order;
+    }
+
+    public ProductEntity getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductEntity product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPk that = (OrderItemPk) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
